8efd60e176cf6ce3d0a2ddbcb2c188a739fa5492
Bedasa Wayessa <bedonaf@gmail.com>
2023-10-23 03:42:13 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     usr_id = req.get(url + 'users/{}'.format(sys.argv[1])).json()
     to_do = req.get(url + 'todos', params={'userId': sys.argv[1]}).json()
     # print(to_do)
-    completed = [title.get("title") for title in to_do if title.get('completed') is True]
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
     print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),len(completed),len(to_do)))
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
     [print("\t {}".format(title)) for title in completed]
